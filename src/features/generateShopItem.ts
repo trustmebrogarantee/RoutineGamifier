e125549963273b19ae2060940cbaaa34ffa35a55
Oleg Shipitsyn <olegshipitsyn.work@gmail.com>
2024-12-28 14:34:48 +0300

    chore: replace iamge url

--- src/features/generateShopItem.ts
+++ src/features/generateShopItem.ts
@@ -5,5 +5,5 @@ import { RoutineGamifier } from '@shared/types/RoutineGamifier';
 export const generateShopItem = (): RoutineGamifier.ShopItem => {
   const price = Math.round(Math.random() * 100 + 1) + 5
   const name = pickRandom<string>(['Поесть в ресторане', 'Поиграть 2 часа', 'Посмотреть аниме', 'Вкинуться'])
-  return { name, price: price - (price % 5), image: 'https://random.imagecdn.app/500/150', id: randomUid() }
+  return { name, price: price - (price % 5), image: 'https://fakeimg.pl/600x400', id: randomUid() }
 }
