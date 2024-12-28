149fa7d01888d4ed50451ee280701d327e24d29c
Oleg Shipitsyn <olegshipitsyn.work@gmail.com>
2024-12-28 14:36:43 +0300

    chore: consistent props

--- src/widgets/Shop.tsx
+++ src/widgets/Shop.tsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { RoutineGamifier } from '@shared/types/RoutineGamifier';
 import React from 'react';
 
-interface Props {
+type Props = {
   coins: RoutineGamifier.Coins,
   shopItems: RoutineGamifier.ShopItem[],
   purchaseItem: (item: RoutineGamifier.ShopItem) => void,
