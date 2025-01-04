a1dd9cbc199f5c63298bc938e5abc03919aedade
Oleg Shipitsyn <olegshipitsyn.work@gmail.com>
2025-01-04 18:12:48 +0300

    fix(useStoredState): store initial state too

--- src/shared/useStoredState.ts
+++ src/shared/useStoredState.ts
@@ -3,6 +3,9 @@ import { safeLocalStorage } from "@shared/safeLocalStorage"
 
 export const useStoredState = (key: string, initialValue: unknown) => {
   const [value, _setValue] = useState(() => safeLocalStorage.get(key, initialValue))
+  
+  safeLocalStorage.set(key, value)
+
   const setValue = (newValue: unknown) => {
     newValue = typeof newValue === 'function' ? newValue(value) : newValue
     safeLocalStorage.set(key, newValue)
